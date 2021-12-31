b20c48fc363831cf12311fe2a408be86acdd5ae6
AlexTrinityBlock <q123717111@gmail.com>
2022-01-01 01:32:37 +0800

    t

--- util/CryptUtil.py
+++ util/CryptUtil.py
@@ -2,6 +2,9 @@ import hashlib
 from Crypto.Cipher import AES, PKCS1_OAEP
 import base64
 from Crypto.Util import Counter
+import json
+from base64 import b64encode
+from base64 import b64decode
 
 def hash256(KeyStr: str):
     byteString = bytes(KeyStr, "utf-8")
@@ -18,14 +21,38 @@ def Base64StringToBytes(base64String):
     message_bytes = base64.b64decode(base64_bytes)
     return message_bytes
 
-def encrptToBase64(plaintextString:str, key):
-    plaintextBytes = bytes(plaintextString, 'utf-8')
-    cipher = AES.new(key, AES.MODE_CTR, counter=Counter.new(128))
-    cipherBytes=cipher.encrypt(plaintextBytes)
-    return bytesToBase64String(cipherBytes)
+# def encrptToBase64(plaintextString:str, key):
+#     plaintextBytes = bytes(plaintextString, 'utf-8')
+#     cipher = AES.new(key, AES.MODE_CTR, counter=Counter.new(128))
+#     cipherBytes=cipher.encrypt(plaintextBytes)
+#     return bytesToBase64String(cipherBytes)
 
-def decrptFromBase64toString(ciphertextBase64, key):
+def encrptToBase64(plaintextString:str, key):
+    header =b"Good Samaritan"
+    plaintextBytes = bytes(plaintextString, 'utf-8')
+    cipher = AES.new(key, AES.MODE_GCM)
+    cipher.update(header)
+    cipherBytes,tag=cipher.encrypt_and_digest(plaintextBytes)
+    json_k = [ 'nonce', 'header', 'ciphertext', 'tag' ]
+    json_v = [ b64encode(x).decode('utf-8') for x in [cipher.nonce, header, cipherBytes, tag ]]
+    result = json.dumps(dict(zip(json_k, json_v)))
+    return bytesToBase64String(bytes(result,"utf-8"))
+
+
+# def decrptFromBase64toString(ciphertextBase64, key):
+#     ciphertextBytes=Base64StringToBytes(ciphertextBase64)
+#     cipher = AES.new(key, AES.MODE_CTR, counter=Counter.new(128))
+#     plainBytes:bytes= cipher.decrypt(ciphertextBytes)
+#     return plainBytes.decode('utf-8')
+
+def decrptFromBase64toString(ciphertextBase64:bytes, key):
     ciphertextBytes=Base64StringToBytes(ciphertextBase64)
-    cipher = AES.new(key, AES.MODE_CTR, counter=Counter.new(128))
-    plainBytes:bytes= cipher.decrypt(ciphertextBytes)
-    return plainBytes.decode('utf-8')
+    cipherGCMInfoJSON=ciphertextBytes.decode("utf-8")
+
+    b64 = json.loads(cipherGCMInfoJSON)
+    json_k = [ 'nonce', 'header', 'ciphertext', 'tag' ]
+    jv = {k:b64decode(b64[k]) for k in json_k}
+    cipher = AES.new(key, AES.MODE_GCM, nonce=jv['nonce'])
+    cipher.update(jv['header'])
+    plaintext:bytes = cipher.decrypt_and_verify(jv['ciphertext'], jv['tag'])
+    return plaintext.decode("utf-8")
